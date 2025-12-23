d895faa3a723e0d17e3f1c47f40e7b0f0ad94515
CodingDaniel1 <cuberdanielchen@gmail.com>
2025-12-23 22:24:50 +0800

    Yippe

--- examples/bindless.rs
+++ examples/bindless.rs
@@ -7,7 +7,7 @@ fn main() {
         .format(slang::CompileTarget::Spirv)
         .profile(global.find_profile("spirv_1_5"))];
 
-    let options = slang::CompilerOptions::default().bindless_space_index(2);
+    let options = slang::CompilerOptions::default();
 
     let session = global
         .create_session(&SessionDesc::default().targets(&targets).options(&options))
